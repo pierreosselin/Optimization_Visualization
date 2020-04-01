2506fa5cfc0e3a77727bbe9ca328db5b2a46d0b8
pierreosselin <pierre.osselin@gmail.com>
2020-04-01 23:09:25 +0200

    1d plot with optimization

--- graph2d.js
+++ graph2d.js
@@ -1,11 +1,21 @@
 
-const objective = x => x**2;
+const objective2 = x => x**2;
 const nPoints = 100;
 const intervalB = -1;
 const intervalU = 1;
 // create data
 //const data = [{x: 0, y: 20}, {x: 150, y: 150}, {x: 300, y: 100}, {x: 450, y: 20}, {x: 600, y: 130}]
-const data = [...Array(nPoints).keys()].map(index => ({x: (index/(nPoints-1))*(intervalU - intervalB) + intervalB, y: objective((index/(nPoints-1))*(intervalU - intervalB) + intervalB)}));
+
+const data = [...Array(nPoints).keys()].map(index => ({x: (index/(nPoints-1))*(intervalU - intervalB) + intervalB, y: objective2((index/(nPoints-1))*(intervalU - intervalB) + intervalB)}));
+
+// Perform Optimization
+const alg = new GradientDescent(objective, 0.01, [1], 0.9)
+const result = alg.optimize(eps, nlim)
+const dataOpti = result.map(index => ({x: index[0], y: objective(index)}));
+console.log(result);
+console.log(dataOpti);
+
+
 const width = 700;
 const height = 500;
 const margin = {top: 10, right: 10, bottom: 30, left: 10};
@@ -29,7 +39,9 @@ var curveFunc = d3.line()
   .x(({ x }) => xScale(x))
   .y(({ y }) => yScale(y));
 
-
+var lineFunc = d3.line()
+  .x(({ x }) => xScale(x))
+  .y(({ y }) => yScale(y));
 
 // Add the path using this helper function
 svg.append('path')
@@ -37,6 +49,10 @@ svg.append('path')
   .attr('stroke', 'black')
   .attr('fill', 'none');
 
+svg.append('path')
+  .attr('d', lineFunc(dataOpti))
+  .attr('stroke', 'red')
+  .attr('fill', 'none');
 
 const xAxis = g => g
     .attr("transform", `translate(0,${height - margin.bottom})`)
