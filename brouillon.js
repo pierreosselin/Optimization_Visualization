540e061c9195a75d8f78d9833a72951d05e171ef
pierreosselin <pierre.osselin@gmail.com>
2020-04-01 21:50:02 +0200

    2d plot

---
+++ brouillon.js
@@ -0,0 +1,41 @@
+const data = [...Array(100).keys()].map(index => ({ abs: index, ord: index**2 }));
+//const data = [{abs : 0.1, ord: 0.2}, {abs : 1, ord: 0.1}, {abs : 2, ord: 3}];
+const width = 500;
+const height = 500;
+const margin = {top: 20, right: 30, bottom: 30, left: 40};
+
+const x = d3.scaleLinear()
+    .domain([0, Math.max(...data.map(({abs}) => abs))]).nice()
+    .range([margin.left, width - margin.right]);
+
+const y = d3.scaleLinear()
+    .domain([0, Math.max(...data.map(({ord}) => ord))]).nice()
+    .range([height - margin.bottom, margin.top]);
+
+const line = d3.line()
+    .x(({ abs }) => x(abs))
+    .y(({ ord }) => y(ord));
+//    .interpolate("linear");
+
+const svg = d3.select('#svg1')
+    .append("svg")
+    .attr("viewBox", [0, 0, width, height]);
+
+svg.append("path")
+    .datum(data)
+    .attr("stroke", "steelblue", fill="none")
+    .attr("d", line);
+
+const xAxis = g => g
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(d3.axisBottom(x));
+
+const yAxis = g => g
+    .attr("transform", `translate(${margin.left},0)`)
+    .call(d3.axisLeft(y));
+
+svg.append("g")
+    .call(xAxis);
+
+svg.append("g")
+    .call(yAxis);
--- graph.js
+++ graph.js
@@ -1,35 +1,50 @@
-const data = [...Array(100).keys()].map(index => ({ abs: index, ord: index**2 }));
-const width = 500;
-const height = 500;
-const margin = {top: 20, right: 30, bottom: 30, left: 40};
 
-const x = d3.scaleLinear()
-    .domain([0, Math.max(...data.map(({abs}) => abs))]).nice()
+const objective = x => x**2;
+const nPoints = 100;
+const intervalB = -1;
+const intervalU = 1;
+// create data
+//const data = [{x: 0, y: 20}, {x: 150, y: 150}, {x: 300, y: 100}, {x: 450, y: 20}, {x: 600, y: 130}]
+const data = [...Array(nPoints).keys()].map(index => ({x: (index/(nPoints-1))*(intervalU - intervalB) + intervalB, y: objective((index/(nPoints-1))*(intervalU - intervalB) + intervalB)}));
+const width = 700;
+const height = 500;
+const margin = {top: 10, right: 10, bottom: 30, left: 10};
+
+const xScale = d3.scaleLinear()
+    .domain([Math.min(...data.map(({x}) => x)), Math.max(...data.map(({x}) => x))]).nice()
     .range([margin.left, width - margin.right]);
 
-const y = d3.scaleLinear()
-    .domain([0, Math.max(...data.map(({ord}) => ord))]).nice()
+const yScale = d3.scaleLinear()
+    .domain([Math.min(...data.map(({y}) => y)), Math.max(...data.map(({y}) => y))]).nice()
     .range([height - margin.bottom, margin.top]);
 
-const line = d3.line()
-    .x(({ abs }) => x(abs))
-    .y(({ ord }) => y(ord));
 
-const svg = d3.select('#svg1')
-    .attr("viewBox", [0, 0, width, height]);
+// create svg element:
+var svg = d3.select("#svg1").append("svg")
+  .attr("viewBox", [0, 0, width, height]);
 
-svg.append("path")
-    .datum(data)
-    .attr("stroke", "steelblue", fill="none")
-    .attr("d", line);
+// prepare a helper function
+var curveFunc = d3.line()
+  .curve(d3.curveBasis)              // This is where you define the type of curve. Try curveStep for instance.
+  .x(({ x }) => xScale(x))
+  .y(({ y }) => yScale(y));
+
+
+
+// Add the path using this helper function
+svg.append('path')
+  .attr('d', curveFunc(data))
+  .attr('stroke', 'black')
+  .attr('fill', 'none');
+
 
 const xAxis = g => g
     .attr("transform", `translate(0,${height - margin.bottom})`)
-    .call(d3.axisBottom(x));
+    .call(d3.axisBottom(xScale));
 
 const yAxis = g => g
     .attr("transform", `translate(${margin.left},0)`)
-    .call(d3.axisLeft(y));
+    .call(d3.axisLeft(yScale));
 
 svg.append("g")
     .call(xAxis);
