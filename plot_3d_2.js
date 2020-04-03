2c883b1369bbcadb9375104b2e3a22c951ae831d
Corentin Carteau <corentin.carteau.intern@birdie.care>
2020-04-03 11:01:21 +0200

    clarify variable naming in contour plot example

--- plot_3d_2.js
+++ plot_3d_2.js
@@ -1,10 +1,11 @@
 const value = (x, y) =>
     (1 + (x + y + 1) ** 2 * (19 - 14 * x + 3 * x ** 2 - 14 * y + 6 * x * y + 3 * y ** 2))
     * (30 + (2 * x - 3 * y) ** 2 * (18 - 32 * x + 12 * x * x + 48 * y - 36 * x * y + 27 * y ** 2));
-const height = 600;
-const width = 600;
+const height = 600; // pixels
+const width = 600; // pixels
 
-const x = d3.scaleLinear([-2, 2], [0, width + 28]);
+// scale linear takes domain (original x and y unit) and range (mapped pixel range)
+const x = d3.scaleLinear([-2, 2], [0, width]);
 const y = d3.scaleLinear([-2, 1], [height, 0]);
 
 const xAxis = g => g
@@ -19,52 +20,51 @@ const yAxis = g => g
     .call(g => g.select(".domain").remove())
     .call(g => g.selectAll(".tick").filter(d => y.domain().includes(d)).remove());
 
-const svg = d3.select('#svg1').attr("viewBox", [0, 0, width + 28, height])
+const svg = d3.select('#svg1').attr("viewBox", [0, 0, width, height])
     .style("display", "block")
     .style("margin", "0 -14px")
-    .style("width", "calc(100% + 28px)");
+    .style("width", "calc(100%)");
 
 
 const thresholds = d3.range(1, 20).map(i => Math.pow(2, i));
 const color = d3.scaleSequentialLog(d3.extent(thresholds), d3.interpolateMagma);
 
 function _makeGrid() {
-    const q = 4; // The level of detail, e.g., sample every 4 pixels in x and y.
-    const x0 = -q / 2, x1 = width + 28 + q;
-    const y0 = -q / 2, y1 = height + q;
-    const n = Math.ceil((x1 - x0) / q);
-    const m = Math.ceil((y1 - y0) / q);
-    const grid = new Array(n * m);
-    for (let j = 0; j < m; ++j) {
-        for (let i = 0; i < n; ++i) {
-            grid[j * n + i] = value(x.invert(i * q + x0), y.invert(j * q + y0));
+    const precision = 4; // The level of detail, e.g., sample every 4 pixels in x and y.
+    const gridWidth = Math.ceil(width / precision);
+    const gridHeight = Math.ceil(height / precision);
+    const grid = new Array(gridWidth * gridHeight);
+
+    for (let j = 0; j < gridHeight; ++j) {
+        for (let i = 0; i < gridWidth; ++i) {
+            grid[j * gridWidth + i] = value(x.invert(i * precision), y.invert(j * precision));
         }
     }
-    grid.x = -q;
-    grid.y = -q;
-    grid.k = q;
-    grid.n = n;
-    grid.m = m;
+    grid.xOrigin = 0;
+    grid.yOrigin = 0;
+    grid.precision = precision;
+    grid.width = gridWidth;
+    grid.heigth = gridHeight;
     return grid;
 }
 
 const grid = _makeGrid();
-const transform = ({type, value, coordinates}) => {
+const gridCoordsToPixelCoords = ({type, value, coordinates}) => {
     return {type, value, coordinates: coordinates.map(rings => {
             return rings.map(points => {
                 return points.map(([x, y]) => ([
-                    grid.x + grid.k * x,
-                    grid.y + grid.k * y
+                    grid.xOrigin + grid.precision * x,
+                    grid.yOrigin + grid.precision * y
                 ]));
             });
         })};
 };
 
 const contours = d3.contours()
-    .size([grid.n, grid.m])
+    .size([grid.width, grid.heigth])
     .thresholds(thresholds)
     (grid)
-    .map(transform);
+    .map(gridCoordsToPixelCoords);
 
 svg.append("g")
     .attr("fill", "none")
