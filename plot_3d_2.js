3c7a76366d36c792bdc5ebb938a2bb8739972514
pierreosselin <pierre.osselin@gmail.com>
2020-04-03 09:27:04 +0200

    Add Rosenbrock

--- plot_3d_2.js
+++ plot_3d_2.js
@@ -1,11 +1,16 @@
 const value = (x, y) =>
     (1 + (x + y + 1) ** 2 * (19 - 14 * x + 3 * x ** 2 - 14 * y + 6 * x * y + 3 * y ** 2))
     * (30 + (2 * x - 3 * y) ** 2 * (18 - 32 * x + 12 * x * x + 48 * y - 36 * x * y + 27 * y ** 2));
+
+const rosenbrock = (x,y) =>
+  (1-x)**2  + 100*(y - x**2)**2 + 1
+
+
 const height = 600;
 const width = 600;
 
-const x = d3.scaleLinear([-2, 2], [0, width + 28]);
-const y = d3.scaleLinear([-2, 1], [height, 0]);
+const x = d3.scaleLinear([-2, 3], [0, width + 28]);
+const y = d3.scaleLinear([-2, 3], [height, 0]);
 
 const xAxis = g => g
     .attr("transform", `translate(0,${height})`)
@@ -25,9 +30,10 @@ const svg = d3.select('#svg1').attr("viewBox", [0, 0, width + 28, height])
     .style("width", "calc(100% + 28px)");
 
 
-const thresholds = d3.range(1, 20).map(i => Math.pow(2, i));
-const color = d3.scaleSequentialLog(d3.extent(thresholds), d3.interpolateMagma);
+const thresholds = d3.range(0, 20).map(i => Math.pow(2, i));
 
+const color = d3.scaleSequentialLog(d3.extent(thresholds), d3.interpolateYlGnBu);
+console.log(d3.extent(thresholds));
 function _makeGrid() {
     const q = 4; // The level of detail, e.g., sample every 4 pixels in x and y.
     const x0 = -q / 2, x1 = width + 28 + q;
@@ -37,7 +43,7 @@ function _makeGrid() {
     const grid = new Array(n * m);
     for (let j = 0; j < m; ++j) {
         for (let i = 0; i < n; ++i) {
-            grid[j * n + i] = value(x.invert(i * q + x0), y.invert(j * q + y0));
+            grid[j * n + i] = rosenbrock(x.invert(i * q + x0), y.invert(j * q + y0));
         }
     }
     grid.x = -q;
