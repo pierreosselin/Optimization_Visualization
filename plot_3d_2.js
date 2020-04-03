36686e0b1142e389d66814f4f61e4b51b4622fb0
Corentin Carteau <corentin.carteau.intern@birdie.care>
2020-04-03 11:41:24 +0200

    move contour plot into separate function with documented interface

--- plot_3d_2.js
+++ plot_3d_2.js
@@ -4,80 +4,100 @@ const value = (x, y) =>
 const height = 600; // pixels
 const width = 600; // pixels
 
-// scale linear takes domain (original x and y unit) and range (mapped pixel range)
-const x = d3.scaleLinear([-2, 2], [0, width]);
-const y = d3.scaleLinear([-2, 1], [height, 0]);
+const square = (x, y) => x**2 + y**2;
 
-const xAxis = g => g
-    .attr("transform", `translate(0,${height})`)
-    .call(d3.axisTop(x).ticks(width / height * 10))
-    .call(g => g.select(".domain").remove())
-    .call(g => g.selectAll(".tick").filter(d => x.domain().includes(d)).remove());
+/**
+ * Display a contour plot of a function over a domain.
+ * @param svg - d3 svg object where to insert the plot.
+ * @param f - 2 variables function with real values to plot.
+ * @param xDomain - 2 elements list, domain of the first variable of f for the plot.
+ * @param yDomain - 2 elements list, domain of the second variable of f for the plot.
+ * @param width - pixels width of the plot.
+ * @param height - pixels height of the plot.
+ * @param precision - sample every <precision> pixels in xDomain and yDomain.
+ */
+function contourPlot(svg, f, xDomain, yDomain, width, height, precision) {
+    /**
+     * Compute grid with values to plot.
+     */
+    const x = d3.scaleLinear(xDomain, [0, width]);
+    const y = d3.scaleLinear(yDomain, [height, 0]);
 
-const yAxis = g => g
-    .attr("transform", "translate(-1,0)")
-    .call(d3.axisRight(y))
-    .call(g => g.select(".domain").remove())
-    .call(g => g.selectAll(".tick").filter(d => y.domain().includes(d)).remove());
-
-const svg = d3.select('#svg1').attr("viewBox", [0, 0, width, height])
-    .style("display", "block")
-    .style("margin", "0 -14px")
-    .style("width", "calc(100%)");
-
-
-const thresholds = d3.range(1, 20).map(i => Math.pow(2, i));
-const color = d3.scaleSequentialLog(d3.extent(thresholds), d3.interpolateMagma);
-
-function _makeGrid() {
-    const precision = 4; // The level of detail, e.g., sample every 4 pixels in x and y.
     const gridWidth = Math.ceil(width / precision);
     const gridHeight = Math.ceil(height / precision);
     const grid = new Array(gridWidth * gridHeight);
-
     for (let j = 0; j < gridHeight; ++j) {
         for (let i = 0; i < gridWidth; ++i) {
-            grid[j * gridWidth + i] = value(x.invert(i * precision), y.invert(j * precision));
+            grid[j * gridWidth + i] = f(x.invert(i * precision), y.invert(j * precision));
         }
     }
-    grid.xOrigin = 0;
-    grid.yOrigin = 0;
-    grid.precision = precision;
-    grid.width = gridWidth;
-    grid.heigth = gridHeight;
-    return grid;
+
+
+    /**
+     * Map grid values to pixel coordinates and define color map.
+     */
+    const gridCoordsToPixelCoords = ({type, value, coordinates}) => {
+        return {
+            type,
+            value,
+            coordinates: coordinates.map(rings => {
+                return rings.map(points => {
+                    return points.map(([x, y]) => ([
+                        precision * x,
+                        precision * y
+                    ]));
+                });
+        })};
+    };
+
+    // TODO customize threshold
+    const thresholds = d3.range(1, 20).map(i => Math.pow(2, i));
+    const color = d3.scaleSequentialLog(d3.extent(thresholds), d3.interpolateMagma);
+    const contours = d3.contours()
+        .size([gridWidth, gridHeight])
+        .thresholds(thresholds)
+        (grid)
+        .map(gridCoordsToPixelCoords);
+
+    /**
+     * Display plot in svg
+     */
+    svg
+        .attr("viewBox", [0, 0, width, height])
+        .style("display", "block")
+        .style("margin", "0 -14px")
+        .style("width", "calc(100%)")
+        .append("g")
+        .attr("fill", "none")
+        .attr("stroke", "#fff")
+        .attr("stroke-opacity", 0.5)
+        .selectAll("path")
+        .data(contours)
+        .join("path")
+        .attr("fill", d => color(d.value))
+        .attr("d", d3.geoPath());
+
+    /**
+     * Add axis.
+     */
+    const xAxis = g => g
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisTop(x).ticks(width / height * 10))
+        .call(g => g.select(".domain").remove())
+        .call(g => g.selectAll(".tick").filter(d => x.domain().includes(d)).remove());
+
+    const yAxis = g => g
+        .attr("transform", "translate(-1,0)")
+        .call(d3.axisRight(y))
+        .call(g => g.select(".domain").remove())
+        .call(g => g.selectAll(".tick").filter(d => y.domain().includes(d)).remove());
+
+    svg.append("g")
+        .call(xAxis);
+
+    svg.append("g")
+        .call(yAxis);
+
 }
 
-const grid = _makeGrid();
-const gridCoordsToPixelCoords = ({type, value, coordinates}) => {
-    return {type, value, coordinates: coordinates.map(rings => {
-            return rings.map(points => {
-                return points.map(([x, y]) => ([
-                    grid.xOrigin + grid.precision * x,
-                    grid.yOrigin + grid.precision * y
-                ]));
-            });
-        })};
-};
-
-const contours = d3.contours()
-    .size([grid.width, grid.heigth])
-    .thresholds(thresholds)
-    (grid)
-    .map(gridCoordsToPixelCoords);
-
-svg.append("g")
-    .attr("fill", "none")
-    .attr("stroke", "#fff")
-    .attr("stroke-opacity", 0.5)
-    .selectAll("path")
-    .data(contours)
-    .join("path")
-    .attr("fill", d => color(d.value))
-    .attr("d", d3.geoPath());
-
-svg.append("g")
-    .call(xAxis);
-
-svg.append("g")
-    .call(yAxis);
+contourPlot(d3.select('#svg1'), value, [-2, 2], [-2, 2], width, height, 4);
