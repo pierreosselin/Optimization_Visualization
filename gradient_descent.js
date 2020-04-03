ff80adf1a242253f40871e56e7b2bcd6b9e04c06
Corentin Carteau <corentin.carteau.intern@birdie.care>
2020-04-03 14:05:43 +0200

    add example contour plot drawing

--- gradient_descent.js
+++ gradient_descent.js
@@ -1,16 +1,10 @@
-console.log("Gradient Descent Algorithm");
-const h = 0.01;
-const x = [1, 2];
-const eps = 0.1;
-
-const objective = x => x.reduce((acc, value) => (acc + value ** 2), 0);
-
 class GradientDescent {
   constructor(h, objective, x_ini) {
     this.h = h;
     this.objective = objective;
     this.x = x_ini;
     this.delta = 0.1;
+    this.path = [];
   }
 
   differentiate(arr) {
@@ -35,13 +29,36 @@ class GradientDescent {
   }
 
   optimize(eps) {
-    var norm = 0;
+    let norm = 0;
     do {
-      norm = (this.one_step()) ** (1 / 2)
+      this.path.push(this.x.map(x => x));
+      norm = (this.one_step()) ** (1 / 2);
       console.log("Norm:", norm, " State:", this.x);
     } while (norm > eps);
   }
+
+  getPath = () => this.path;
 }
 
-alg = new GradientDescent(h, objective, x)
-alg.optimize(eps)
+const h = 0.01;
+const x = [-30, 175];
+const eps = 0.1;
+
+const thresholds = d3.range(1, 20).map(i => Math.pow(2, i));
+const square = (x, y) => x ** 2 + y ** 2 + (x + y) ** 2;
+
+alg = new GradientDescent(h, ([x, y]) => square(x, y), x);
+alg.optimize(eps);
+
+const contourPlot = new ContourPlot(
+  d3.select('#svg1'),
+  [-200, 200],
+  [-200, 200],
+  600,
+  600,
+);
+
+contourPlot
+  .draw(square, 4, thresholds)
+  .addAxis()
+  .addLine(alg.getPath());
