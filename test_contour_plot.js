acc234212c5bac1d0766d19638ebc89665065fda
pierreosselin <pierre.osselin@gmail.com>
2020-04-04 18:52:35 +0200

    Add function selection

--- test_contour_plot.js
+++ test_contour_plot.js
@@ -4,7 +4,7 @@ function test_square(h, eps, nlim) {
   const square = ([x, y]) => x ** 2 + y ** 2 + (x + y) ** 2;
   const rosenbrock = ([x,y]) => (1-x)**2  + 100*(y - x**2)**2 + 1;
   const allGroup = ["square", "rosenbrock"];
-  const objectives = {"square": {obj: square, x_ini: [-30, 175], delta: 0.1, xDomain : [-200,200], yDomain : [-200,200]}, "rosenbrock": {obj: rosenbrock, x_ini: [2.5, -1.5], delta: 0.0001, xDomain : [-2,3], yDomain : [-2,3]}};
+  const objectives = {"square": {obj: square, x_ini: [-30, 175], delta: 0.1, xDomain : [-200,200], yDomain : [-200,200], interpolation :d3.interpolateMagma}, "rosenbrock": {obj: rosenbrock, x_ini: [2.5, -1.5], delta: 0.0001, xDomain : [-2,3], yDomain : [-2,3], interpolation: d3.interpolateYlGnBu}};
 
   // Create basic constituent of the Visualization (Contour Windows and button)
   // Window for contour
@@ -36,7 +36,7 @@ function test_square(h, eps, nlim) {
 
   // Plot Initial Presentation
   contourPlot
-    .draw(square, 4, thresholds)
+    .draw(objective, 4, thresholds)
     .addAxis()
     .addLine(alg.getPath());
 
@@ -62,7 +62,7 @@ function test_square(h, eps, nlim) {
       600,
     );
     contourPlot
-      .draw(objective, 4, thresholds)
+      .draw(objective, 4, thresholds, objectives[myfunction].interpolation)
       .addAxis()
       .addLine(alg.getPath());
   }
@@ -77,14 +77,14 @@ function test_square(h, eps, nlim) {
 
 
 function test_rosenbrock(h, eps, nlim) {
-  const rosenbrock = (x,y) =>
+  const rosenbrock = ([x,y]) =>
     (1-x)**2  + 100*(y - x**2)**2 + 1;
 
   const x = [2.5, -1.5];
 
   const thresholds = d3.range(1, 20).map(i => Math.pow(2, i));
 
-  const alg = new GradientDescent(([x, y]) => rosenbrock(x, y), h, x, 0.0001);
+  const alg = new GradientDescent(rosenbrock, h, x, 0.0001);
   alg.optimize(eps, nlim);
 
   const contourPlot = new ContourPlot(
@@ -101,5 +101,5 @@ function test_rosenbrock(h, eps, nlim) {
     .addLine(alg.getPath());
 }
 
-//test_square(0.01, 0.1, 20);
-test_rosenbrock(0.01, 0.1, 100);
+test_square(0.01, 0.1, 20);
+//test_rosenbrock(0.01, 0.1, 100);
