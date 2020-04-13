303216652ad6a5d8d4f2dc1141f5ba7e99e35a6c
pierreosselin <pierre.osselin@gmail.com>
2020-04-13 09:59:20 +0200

    Add doc

--- documentationDisplay.js
+++ documentationDisplay.js
@@ -1,5 +1,5 @@
 function documentationFactory() {
   let div = d3.select("#documentation").html("");
   div.insert("p").text("First paragraph");
-  //div.insert("div").attr("lang", "latex").text("x_{k+1} = x_{k} - \alpha_{k} \nabla_{x}f(x_{k})"");
+  div.insert("div").attr("lang", "latex").text("x_{k+1} = x_{k} - \\alpha_{k} \\nabla_{x}f(x_{k})");
 }
