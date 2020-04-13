7ba071c9c9e411b595927517402c781ce74775d6
pierreosselin <pierre.osselin@gmail.com>
2020-04-13 11:04:07 +0200

    Fix MathJax Rendering

--- documentationDisplay.js
+++ documentationDisplay.js
@@ -1,7 +1,8 @@
 function documentationFactory(objFunction, algName) {
   let div = d3.select("#documentation").html("");
-  div = div.append("p");
-  let text = div.append("foreignObject").attr("width",100).attr("height",100)
+  div.append("p").text(objFunction);
+  let formulaP = div.append("p");
+  let text = formulaP.append("foreignObject").attr("width",100).attr("height",100)
   text.text("$$ x = \\sum_{i \\in A} i^{2} $$")
   MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
 }
