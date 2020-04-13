d3827eeae6b4ad699272bc5cd5f143f6177fcb46
pierreosselin <pierre.osselin@gmail.com>
2020-04-13 10:45:43 +0200

    Change Latex to MathJax

--- documentationDisplay.js
+++ documentationDisplay.js
@@ -1,5 +1,5 @@
 function documentationFactory(objFunction, algName) {
   let div = d3.select("#documentation").html("");
   div.insert("p").text(objFunction);
-  div.insert("div").attr("lang", "latex").text("x_{k+1} = x_{k} - \\alpha_{k} \\nabla_{x} f(x_{k})");
+  div.insert("p").text("$$x_{k} = x_{k} - \alpha_{k} \nabla_{x} f(x_{k})$$");
 }
